72600c7adf19021ae0a43de40cfcca0a0e5d5dd4
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-10-06 12:58:20 -0700

    added homepage

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+    return render_template('home.html', msg=files)
+
+files = [
+    { 'title': 'Cat', 'description': '3D Model of Cat' },
+        { 'title': 'Cat', 'description': '3D Model of Cat' },
+]
+
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
 
 if __name__ == '__main__':
     app.run(debug=True)
