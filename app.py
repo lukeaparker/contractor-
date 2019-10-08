aa8cd8efba73d26348bf5aac99b90b5d24813ae5
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-10-08 12:52:03 -0700

    added mock json data

--- app.py
+++ app.py
@@ -2,20 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    """Return homepage."""
-    return render_template('home.html', msg=files)
 
-files = [
-    { 'title': 'Cat', 'description': '3D Model of Cat' },
-        { 'title': 'Cat', 'description': '3D Model of Cat' },
+playlists = [
+    { 'title': 'Cat', 'description': 'Cat .gcode file sliced for ender 3.' },
+    { 'title': 'Building', 'description': 'Building .gcode file sliced for ender 3.' }
 ]
 
-@app.route('/playlists')
+@app.route('/')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('files_index.html', playlists=playlists)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
